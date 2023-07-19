76c99ee13bdb26f8bced501530102a0ec4088232
Adam Ramdan <arshinks12@gmail.com>
2023-07-19 21:30:18 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+ content: ['*.{html,js}'],
+ theme: {
+  container: {
+   center: true,
+   padding: '16px',
+  },
+  extend: {
+   colors: {
+    primary: '#0F8B8D',
+    secondary: '#143642',
+    third: '#A8201A',
+   },
+  },
+ },
+ plugins: [],
+};
